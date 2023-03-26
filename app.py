ea0dd89ed9802a0cb12a974215358903e299d981
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-26 11:02:50 +0100

    added code to ask user if they want to generate a password

---
+++ app.py
@@ -0,0 +1,11 @@
+# ask user if they want to generate password
+def main():
+    option = char(input("Do you want to generate password?\ny/n: "))
+
+# if yes, get password length
+
+# grenerate password
+
+# print password
+
+# if initial response is no, then exit program
