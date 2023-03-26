11a219afc3e851d64758ae2308be436cc7615b2e
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-26 11:05:47 +0100

    added conditional to get length of password if user option is y

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # ask user if they want to generate password
 def main():
-    option = char(input("Do you want to generate password?\ny/n: "))
+    option = char(input("Do you want to generate password?\ny/n: ")).lower()
 
-# if yes, get password length
+    # if yes, get password length
+    if option == 'y':
+        password_len = input("Enter length of password: ")
 
 # grenerate password
 
